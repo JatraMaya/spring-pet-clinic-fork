7ad0661a3ea286905ec1b63d43ebec3277314980
JatraMaya <ahmad.f.iksan@gmail.com>
2021-05-02 20:04:01 +0800

    Initial push

---
+++ src/main/java/io/jatramayalom/springpetclinicfork/SpringPetClinicForkApplication.java
@@ -0,0 +1,13 @@
+package io.jatramayalom.springpetclinicfork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicForkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicForkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jatramayalom/springpetclinicfork/SpringPetClinicForkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jatramayalom.springpetclinicfork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicForkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
